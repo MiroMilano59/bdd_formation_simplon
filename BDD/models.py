cb6420c6e446573139f3ece2901ccaf4bc55d885
Arnaud <arnaud_pesenti@yahoo.fr>
2024-07-11 12:14:41 +0200

    PROJECT UPDATE: Implemented SQLAlchemy model for the database structure. Also implemented a ready-to-use database connector. Also set in place a template for queries in a dedicated file (crud.py)

--- BDD/models.py
+++ BDD/models.py
@@ -1,12 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import String, Date, Numeric, Boolean
 from sqlalchemy import create_engine, PrimaryKeyConstraint, UniqueConstraint
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 
 # INSTANCIATING A DATABASE FRAMEWORK (i.e. a mix of container and base class) 
 SimplonDB = declarative_base()
 
-# HELPER FUNCTIONS (To simplify the use or implementation of the database)
+# HELPER FUNCTIONS (To simplify interactions with the 'simplon' database)
 def foreign_key(target):
     """
     Returns a python list to use as arugment when calling the `column` method.
@@ -56,189 +57,123 @@ def db_connect(url: str = "sqlite:///./simplon.db", **kwargs):
     # FUNCTION OUTPUT (returns a `sessionmaker` object to help DB interactions)
     return sessionmaker(bind=engine)
 
-# # CREATING TABLES OF THE DATABASE
-# class Movies(SimplonDB):
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'movies'
+# CREATING TABLES OF THE DATABASE
+class Formations(SimplonDB):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'formations'
 
-#     # SPECIFIC TABLE COLUMNS
-#     # 1. Main movie characteritics
-#     Id = Column(Integer, primary_key=True, autoincrement=True)
-#     Title = Column(String, nullable=True)
-#     Title_Fr = Column(String, nullable=False)
-#     Synopsis = Column(String, nullable=True)
-#     Duration = Column(Integer, nullable=True)
-#     Poster_URL = Column(String, nullable=True)
-#     Press_Rating = Column(Numeric(precision=2, scale=1), nullable=True)
-#     Public_Rating = Column(Numeric(precision=2, scale=1), nullable=True)
+    # TABLE SPECIFIC COLUMNS
+    # 1. Main movie characteritics
+    Id = Column(Integer, primary_key=True, autoincrement=True)
+    Libelle = Column(String, nullable=False)
+    code_RS = Column(String, nullable=True)
+    Code_RNCP = Column(String, nullable=True)
+    Niveau_Sortie = Column(String, nullable=True)
+    Resume_Programme = Column(String, nullable=True)
 
-#     # 2. Tecnical details
-#     Visa = Column(String, nullable=True)
-#     Awards = Column(String, nullable=True)
-#     Budget = Column(String, nullable=True)
-#     Format = Column(String, nullable=True)            # Whether color or B&W
-#     Category = Column(String, nullable=True)          # Feature film, doc, etc.
-#     Release_Date = Column(Date, nullable=True)
-#     Release_Place = Column(String, nullable=True)
-#     Production_Year = Column(Integer, nullable=True)
+#     # 2. Tecnical details (None so far)
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy model and use)
-#     genres = relationship('Genres', back_populates='movies')
-#     actors = relationship('Actors', back_populates='movies')
-#     countries = relationship('Countries', back_populates='movies')
-#     languages = relationship('Languages', back_populates='movies')
-#     directors = relationship('Directors', back_populates='movies')
-#     distributors = relationship('Distributors', back_populates='movies')
-#     screenwriters = relationship('ScreenWriters', back_populates='movies')
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy model and use)
+    code_nsf = relationship('CodesNSF', back_populates='formation')
+    formacode = relationship('Formacodes', back_populates='formation')
+    session = relationship('Sessions', back_populates='formation')
 
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (UniqueConstraint(*('Title_Fr', 'Release_Date'),
-#                                        name='Title_&_date_is_a_unique_pair'),)
+    # DEFINING SCHEMA SPECIFIC CONSTRAINTS
+    __table_args__ = (UniqueConstraint('Libelle',
+                                       name='Training_name_should_be_unique'),)
 
-# class Persons(SimplonDB):
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'persons'
+class Organismes(SimplonDB):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'organismes'
 
-#     # SPECIFIC TABLE COLUMNS
-#     Id = Column(Integer, primary_key=True, autoincrement=True)
-#     Full_Name = Column(String, nullable=False)
+    # TABLE SPECIFIC COLUMNS
+    # 1. Main movie characteritics
+    Id = Column(Integer, primary_key=True, autoincrement=True)
+    Nom = Column(String, nullable=False)
+    Siret = Column(String, nullable=False)
+    Departement = Column(*foreign_key('Departements.Id'), nullable=False)
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     actor_play = relationship('Actors', back_populates='persons')
-#     film_making = relationship('Directors', back_populates='persons')
-#     screeplay_writing = relationship('ScreenWriters', back_populates='persons')
 
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (UniqueConstraint('Full_Name',
-#                                        name='Full_Name_should_be_unique'),)
+#     # 2. Tecnical details (None so far)
 
-# class PeopleRole(SimplonDB): # Abstract table (for code factorization purpose)
-#     """
-#     Helps factorizing code as it is a common part of other sub classes
-#     """
-#     # RAW PARAMETERS AND SETINGS
-#     __abstract__ = True
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy model and use)
+    session =  relationship('Sessions', back_populates='location')
+    code_dpt = relationship('Departements', back_populates='organisme')
 
-#     # COLUMNS OF THE ABSTRACT TABLE
-#     MovieId = Column(*foreign_key('movies.Id'), nullable=False)
-#     PersonId = Column(*foreign_key('persons.Id'), nullable=False)
+    # DEFINING SCHEMA SPECIFIC CONSTRAINTS
+    __table_args__ = (UniqueConstraint(*('Siret', 'Departement'),
+                                       name='One_center_per_departement'),)
 
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (PrimaryKeyConstraint(*('MovieId', 'PersonId'),
-#                                            name='Composite_primary_key'),)
+class Codes(SimplonDB): # Abstract table (for code factorization purpose)
+    """
+    Helps factorizing code as it is a common part of some other sub classes
+    """
+    # RAW PARAMETERS AND SETINGS
+    __abstract__ = True
 
-# class Actors(PeopleRole):
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'actors'
+    # COLUMNS OF THE ABSTRACT TABLE
+    Formation_Id = Column(*foreign_key('formations.Id'), nullable=False)
+    Code = Column(String, nullable=False)
+    Libelle = Column(String, nullable=True)
 
-#     # SPECIFIC TABLE COLUMNS
-#     Characters = Column(String, nullable=True)
+    # DEFINING SCHEMA SPECIFIC CONSTRAINTS
+    __table_args__ = (PrimaryKeyConstraint(*('Formation_Id', 'Code'),
+                                           name='Composite_primary_key'),)
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='actors')
-#     persons = relationship('Persons', back_populates='actor_play')
+class Formacodes(Codes):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'formacodes'
 
-# class Directors(PeopleRole):
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'directors'
+    # SPECIFIC TABLE COLUMNS
+    Principal = Column(Boolean, nullable=False, default=False)
 
-#     # SPECIFIC TABLE COLUMNS (none at this stage)
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
+    formation = relationship('Formations', back_populates='formacode')
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='directors')
-#     persons = relationship('Persons', back_populates='film_making')
+class CodesNSF(Codes):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'codes_nsf'
 
-# class ScreenWriters(PeopleRole):
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'screenwriters'
+    # SPECIFIC TABLE COLUMNS (none at this stage !)
 
-#     # SPECIFIC TABLE COLUMNS (none at this stage)
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
+    formation = relationship('Formations', back_populates='code_nsf')
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='screenwriters')
-#     persons = relationship('Persons', back_populates='screeplay_writing')
+class Sessions(SimplonDB):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'sessions'
 
-# class Companies(SimplonDB):
-#     """
-#     Table dedicated to companies data. We scraped company name only so far.
+    # SPECIFIC TABLE COLUMNS
+    Id = Column(Integer, primary_key=True, autoincrement=True)
+    Formation_Id = Column(*foreign_key('formations.Id'), nullable=False)
+    organisme_Id = Column(*foreign_key('organismes.Id'), nullable=False)
+    Date_Debut = Column(Date, nullable=True)
+    Type_Session  = Column(String, nullable=True)
+    ville_Session  = Column(String, nullable=True)
 
-#     Potentially, more data could be retrieved for companies (ex: address).
-#     Hence this dedicated table to be used in conjunction with an association
-#     table (i.e. `Distributors`) together with the `Movies` table.
-#     """
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'companies'
 
-#     # SPECIFIC TABLE COLUMNS
-#     Id = Column(Integer, primary_key=True, autoincrement=True)
-#     Full_Name = Column(String, nullable=False)
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
+    formation = relationship('Formations', back_populates='session')
+    location = relationship('Organismes', back_populates='session')
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     distribution = relationship('Distributors', back_populates='companies')
+    # DEFINING SCHEMA SPECIFIC CONSTRAINTS
+    __table_args__ = (UniqueConstraint(*('Formation_Id', 'organisme_Id'),
+                                       name='One_session_per_exact_location'),)
 
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     # At this stage a unique constraint is required to properly manage queries
-#     # This choise is to be reconsidered whenever (if ever) new columns.
-#     __table_args__ = (UniqueConstraint('Full_Name',
-#                                        name='Full_Name_should_be_unique'),)
+class Departements(SimplonDB):
+    # RAW PARAMETERS AND SETINGS
+    __tablename__ = 'departements'
 
-# class Distributors(SimplonDB): 
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'distributors'
+    # SPECIFIC TABLE COLUMNS
+    Id = Column(Integer, primary_key=True) # Ususal french dept. number
+    Nom_Dept = Column(String, nullable=True)
+    Nom_Region = Column(String, nullable=True)
+    Code_Region = Column(Integer, nullable=True)
 
-#     # SPECIFIC TABLE COLUMNS
-#     MovieId = Column(*foreign_key('movies.Id'))
-#     CompId = Column(*foreign_key('companies.Id'))
 
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (PrimaryKeyConstraint(*('MovieId', 'CompId'),
-#                                            name='Composite_primary_key'),)
+    # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
+    organisme = relationship('Organismes', back_populates='code_dept')
 
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='distributors')
-#     companies = relationship('Companies', back_populates='distribution')
-
-# class Genres(SimplonDB): 
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'genres'
-
-#     # SPECIFIC TABLE COLUMNS
-#     MovieId = Column(*foreign_key('movies.Id'))
-#     Genre = Column(String, nullable=False)
-
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (PrimaryKeyConstraint(*('MovieId', 'Genre'),
-#                                            name='Composite_primary_key'),)
-
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='genres')
-
-# class Countries(SimplonDB): 
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'countries'
-
-#     # SPECIFIC TABLE COLUMNS
-#     MovieId = Column(*foreign_key('movies.Id'))
-#     Country = Column(String, nullable=False)
-
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (PrimaryKeyConstraint(*('MovieId', 'Country'),
-#                                            name='Composite_primary_key'),)
-
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='countries')
-
-# class Languages(SimplonDB): 
-#     # RAW PARAMETERS AND SETINGS
-#     __tablename__ = 'languages'
-
-#     # SPECIFIC TABLE COLUMNS
-#     MovieId = Column(*foreign_key('movies.Id'))
-#     Language = Column(String, nullable=False)
-
-#     # DEFINING SCHEMA SPECIFIC CONSTRAINTS
-#     __table_args__ = (PrimaryKeyConstraint(*('MovieId', 'Language'),
-#                                            name='Composite_primary_key'),)
-
-#     # DEFINING PURE ORM RELATIONSHIPS (i.e. enhancing SQLAlchemy features)
-#     movies = relationship('Movies', back_populates='languages')
+    # DEFINING SCHEMA SPECIFIC CONSTRAINTS (None so far)
+    # __table_args__ = (UniqueConstraint(*('Formation_Id', 'organisme_Id'),
+    #                                    name='One_session_per_exact_location'),)
