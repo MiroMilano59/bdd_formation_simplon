ced517672c2dee46149aef439828da09363def0b
MiroMilano59 <153615242+MiroMilano59@users.noreply.github.com>
2024-07-12 10:46:48 +0200

    Feat : updated scraper & update items

--- SCRAPER/crawl_simplon/crawl_simplon/items.py
+++ SCRAPER/crawl_simplon/crawl_simplon/items.py
@@ -6,12 +6,13 @@
 import scrapy
 
 class CrawlSimplonItem(scrapy.Item):
-    intitule_formation = scrapy.Field()
-    formacodes_code = scrapy.Field()
-    intitule_certification = scrapy.Field()
-    date_limite_candidature = scrapy.Field()
-    type_formation = scrapy.Field()
-    lieu = scrapy.Field()
-    date_debut = scrapy.Field()
-    duree = scrapy.Field()
-    niveau_sortie = scrapy.Field()
+    Libelle = scrapy.Field()
+    Formacodes = scrapy.Field()
+    Libele_Certification = scrapy.Field()
+    Date_Limite_Candidature = scrapy.Field()
+    Type_Formation = scrapy.Field()
+    Nom_Dept = scrapy.Field()
+    Date_Debut = scrapy.Field()
+    Duree = scrapy.Field()
+    Niveau_Sortie = scrapy.Field()
+    Resume_Programme = scrapy.Field()
